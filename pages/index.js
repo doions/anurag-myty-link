34ddb2cea124b90558e9d8908fc1a890ef36f978
Doions <43123876+doions@users.noreply.github.com>
2024-04-02 13:00:39 +0530

    Update index.js
    Added Date

--- pages/index.js
+++ pages/index.js
@@ -19,11 +19,18 @@ export default function Home() {
         <p>Dynamic and results-driven entrepreneur with a proven track record of success in building and scaling technology-driven businesses. Seeking a senior leadership position such as CXO where I can leverage my 20 years of experience and innovative mindset to drive organizational growth and profitability.</p>
 
         <h2>Education</h2>
-        <p>Bachelor of Engineering in Computer Science<br />
+        <p>Bachelor of Engineering in Computer Science and Engineering<br />
           Jabalpur Engineering College, Jabalpur<br />
-          CGPA: 8.1/10</p>
+          CGPA: 8.1/10<br />
+          Year: 2004
+    </p>
+   
 
         <h2>Professional Experience</h2>
+        <h3>Founder - SKOOLSOFT</h3>
+        <p>Nov 2003 - Dec 2008<br />
+          - Developed and deployed SKOOLSOFT, a school automation and parent connect platform, in prominent schools across Jabalpur, Mumbai, and Delhi.<br />
+          - Streamlined administrative processes and improved communication between schools and parents, resulting in increased efficiency and satisfaction.</p>
 
         <h3>Founder & CEO, MindsArray Technologies Pvt Ltd (Known as Pay1)</h3>
         <p>Oct 2008 - Nov 2016<br />
@@ -32,11 +39,7 @@ export default function Home() {
           - Led a talented team of professionals to develop innovative solutions, drive business development initiatives, and ensure customer satisfaction.<br />
           - Secured strategic partnerships and alliances to expand market reach and enhance product offerings.</p>
 
-        <h3>SKOOLSOFT</h3>
-        <p>Nov 2003 - Dec 2008<br />
-          - Developed and deployed SKOOLSOFT, a school automation and parent connect platform, in prominent schools across Jabalpur, Mumbai, and Delhi.<br />
-          - Streamlined administrative processes and improved communication between schools and parents, resulting in increased efficiency and satisfaction.</p>
-
+        
         <h3>Founder & CEO, Doions Pvt Ltd</h3>
         <p>Nov 2019 - Dec 2022<br />
           - Established Doions Pvt Ltd to focus on developing cutting-edge products such as Myty and Lopu, catering to Web publishing using AI tools.<br />
@@ -59,11 +62,13 @@ export default function Home() {
             <ul>
               <li>MERN Stack (MongoDB, Express.js, React.js, Node.js)</li>
               <li>LAMP Stack (Linux, Apache, MySQL, PHP)</li>
+              <li>Tools: Figma, VSCode, Postman, Git</li>
+               <li>CICD Pipeline design, Agile methodologies, Jeera</li>
             </ul>
           </li>
           <li>Web Development (HTML, CSS, JavaScript)</li>
           <li>Mobile Development</li>
-          <li>Design</li>
+          <li>Design - Wireframe, UI/UX</li>
           <li>Data Science</li>
           <li>Machine Learning</li>
           <li>Blockchain</li>
